a76ef2b9b72215753dd61cf253c0c28339926d6a
abc1763613206 <abc1763613206@163.com>
2024-07-29 16:35:41 +0800

    chore: format

--- static/js/board.js
+++ static/js/board.js
@@ -1,48 +1,49 @@
-
 function getQueryString(name) {
-    let reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
-    let r = window.location.search.substr(1).match(reg);
-    if (r != null) {
-        return decodeURIComponent(r[2]);
-    };
-    return null;
- }
-function dorank(){
-	var count=0,lastkey="",lastrank=0;
-	$("#board tr:not(.hidden) td.rank").each(function(i){
-		var row=$(this).closest('tr');
-		var key=row.find('.solved').text()+"|"+row.find('.penalty').text();
-		if(row.is('.unofficial')){
-			$(this).html('*');
-			return;
-		}
-		count+=1;
-		if(key != lastkey)
-			lastrank=count;
-		lastkey=key;
-		$(this).html(lastrank);
-	});
+  let reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+  let r = window.location.search.substr(1).match(reg);
+  if (r != null) {
+    return decodeURIComponent(r[2]);
+  }
+  return null;
+}
+function dorank() {
+  var count = 0,
+    lastkey = "",
+    lastrank = 0;
+  $("#board tr:not(.hidden) td.rank").each(function (i) {
+    var row = $(this).closest("tr");
+    var key = row.find(".solved").text() + "|" + row.find(".penalty").text();
+    if (row.is(".unofficial")) {
+      $(this).html("*");
+      return;
+    }
+    count += 1;
+    if (key != lastkey) lastrank = count;
+    lastkey = key;
+    $(this).html(lastrank);
+  });
 
-	var schoolDict = {};
-	count = 0; lastkey = ""; lastrank = 0;
-	$("#board tr:not(.hidden) td.schoolrank").each(function(i){
-		var row=$(this).closest('tr');
-		var key=row.find('.solved').text()+"|"+row.find('.penalty').text();
-		var schoolName = row.find('.school').text();
-		if(row.is('.unofficial') || typeof schoolDict[schoolName] != 'undefined'){
-			$(this).html('-');
-			return;
-		}
-		count+=1;
-		schoolDict[schoolName] = count;
-		if(key != lastkey)
-			lastrank=count;
-		lastkey=key;
-		$(this).html(lastrank);
-	});
+  var schoolDict = {};
+  count = 0;
+  lastkey = "";
+  lastrank = 0;
+  $("#board tr:not(.hidden) td.schoolrank").each(function (i) {
+    var row = $(this).closest("tr");
+    var key = row.find(".solved").text() + "|" + row.find(".penalty").text();
+    var schoolName = row.find(".school").text();
+    if (row.is(".unofficial") || typeof schoolDict[schoolName] != "undefined") {
+      $(this).html("-");
+      return;
+    }
+    count += 1;
+    schoolDict[schoolName] = count;
+    if (key != lastkey) lastrank = count;
+    lastkey = key;
+    $(this).html(lastrank);
+  });
 }
 
-var url=decodeURI(window.location.href);
+var url = decodeURI(window.location.href);
 
 //var scrollParam=url.match(/.+[?&]scroll=(\d+).*/);
 //if(scrollParam != null) scrollParam=parseInt(scrollParam[1],10);
@@ -52,57 +53,63 @@ var url=decodeURI(window.location.href);
 //if(refreshParam != null) refreshParam=parseInt(refreshParam[1],10);
 //else refreshParam=5;
 
-
-var scrollParam = getQueryString('scroll');
-var filterParam = getQueryString('filter');
-var refreshParam = getQueryString('refresh');
-if(scrollParam != null) scrollParam = parseInt(scrollParam);
-if(refreshParam != null) refreshParam = parseInt(refreshParam);
+var scrollParam = getQueryString("scroll");
+var filterParam = getQueryString("filter");
+var refreshParam = getQueryString("refresh");
+if (scrollParam != null) scrollParam = parseInt(scrollParam);
+if (refreshParam != null) refreshParam = parseInt(refreshParam);
 else refreshParam = 5;
-function scrollPage(){
-	var len=$('#board tr').length;
-	var dis=$("#tbottom").offset().top;
-	$('html, body').animate({
-	   scrollTop: dis
-	}, dis*scrollParam,function(){
-		$('html, body').animate({
-		   scrollTop: $("#ttop").offset().top
-		}, len *20, function(){
-			setTimeout(refresh,refreshParam*1000);
-		})
-	});
+function scrollPage() {
+  var len = $("#board tr").length;
+  var dis = $("#tbottom").offset().top;
+  $("html, body").animate(
+    {
+      scrollTop: dis,
+    },
+    dis * scrollParam,
+    function () {
+      $("html, body").animate(
+        {
+          scrollTop: $("#ttop").offset().top,
+        },
+        len * 20,
+        function () {
+          setTimeout(refresh, refreshParam * 1000);
+        }
+      );
+    }
+  );
 }
 
-function refresh(){
-	location.reload(true);
+function refresh() {
+  location.reload(true);
 }
 
-
-
-if(!localStorage.concerned){
-	localStorage.concerned="[]";
+if (!localStorage.concerned) {
+  localStorage.concerned = "[]";
 }
-var concerned=JSON.parse(localStorage.concerned);
+var concerned = JSON.parse(localStorage.concerned);
 
-
-$("td.untried").each(function(i){
-	$(this).attr("onclick","popdown()");
+$("td.untried").each(function (i) {
+  $(this).attr("onclick", "popdown()");
 });
 
-$("td.prob_d").filter(":not(.untried)").each(function(i){
-	$(this).attr("onclick","showsub(this)");
-});
+$("td.prob_d")
+  .filter(":not(.untried)")
+  .each(function (i) {
+    $(this).attr("onclick", "showsub(this)");
+  });
 
-function concernedok(){
-	$('tr.concerned').each(function(){
-		$(this).removeClass('concerned');
-	});
-	concerned.forEach(function(v,i){
-		$("#"+v).addClass('concerned');
-	});
+function concernedok() {
+  $("tr.concerned").each(function () {
+    $(this).removeClass("concerned");
+  });
+  concerned.forEach(function (v, i) {
+    $("#" + v).addClass("concerned");
+  });
 }
-$("td.team").each(function(i){
-	$(this).attr("onclick","showteam(this)");
+$("td.team").each(function (i) {
+  $(this).attr("onclick", "showteam(this)");
 });
 
 //var timeElapsed=$('#time_elapsed').attr('sec');
@@ -112,104 +119,111 @@ $("td.team").each(function(i){
 //$('#time_elapsed').append(secondsToTime(timeElapsed));
 
 concernedok();
-if(filterParam != null) {
-	$("#board tbody tr:not(."+filterParam+")").each(function(){
-		$(this).addClass('hidden');
-	});
+if (filterParam != null) {
+  $("#board tbody tr:not(." + filterParam + ")").each(function () {
+    $(this).addClass("hidden");
+  });
 }
 dorank();
-function concernclick(o){
-	var tid=$(o).attr('tid');
-	var k=concerned.indexOf(tid);
-	var tr=$("#"+tid);
-	if( $(o).is(':checked')){
-		if(k==-1){
-			concerned.push(tid);
-			tr.addClass('concerned');
-		}
-	}else{
-		if(k!=-1){
-			concerned.splice(k,1);
-			tr.removeClass('concerned');
-		}
-	}
-	localStorage.concerned=JSON.stringify(concerned);
+function concernclick(o) {
+  var tid = $(o).attr("tid");
+  var k = concerned.indexOf(tid);
+  var tr = $("#" + tid);
+  if ($(o).is(":checked")) {
+    if (k == -1) {
+      concerned.push(tid);
+      tr.addClass("concerned");
+    }
+  } else {
+    if (k != -1) {
+      concerned.splice(k, 1);
+      tr.removeClass("concerned");
+    }
+  }
+  localStorage.concerned = JSON.stringify(concerned);
 }
 
+var exportLink = document.createElement("a");
+exportLink.href = "#";
+exportLink.innerHTML = "导出数据";
 
-
-var exportLink = document.createElement('a');
-exportLink.href = '#';
-exportLink.innerHTML = '导出数据';
-
-var exportTextArea = document.createElement('textarea');
+var exportTextArea = document.createElement("textarea");
 exportTextArea.rows = 20;
 exportTextArea.cols = 80;
-exportTextArea.style.display = 'none';
+exportTextArea.style.display = "none";
 
-var mainDiv = document.getElementById('maindiv');
-var boardTable = document.getElementById('board');
+var mainDiv = document.getElementById("maindiv");
+var boardTable = document.getElementById("board");
 
 mainDiv.insertBefore(exportLink, boardTable);
 mainDiv.insertBefore(exportTextArea, boardTable);
 
-exportLink.onclick = function() {
-	if (exportTextArea.style.display == 'none') {
-		exportTextArea.innerHTML = genCfGhostData();
-		exportTextArea.style.display = 'block';
-	} else {
-		exportTextArea.style.display = 'none';
-	}
-}
+exportLink.onclick = function () {
+  if (exportTextArea.style.display == "none") {
+    exportTextArea.innerHTML = genCfGhostData();
+    exportTextArea.style.display = "block";
+  } else {
+    exportTextArea.style.display = "none";
+  }
+};
 
 function genCfGhostData() {
-	var result = `@contest "${document.getElementById('contest_title').innerHTML}"\n`;
-	result += `@contlen ${parseInt(document.getElementById('time_elapsed').getAttribute('sec')) / 60}\n`;
-	var numProbs = document.getElementsByClassName('prob_h').length / 2;
-	result += `@problems ${numProbs}\n`;
+  var result = `@contest "${
+    document.getElementById("contest_title").innerHTML
+  }"\n`;
+  result += `@contlen ${
+    parseInt(document.getElementById("time_elapsed").getAttribute("sec")) / 60
+  }\n`;
+  var numProbs = document.getElementsByClassName("prob_h").length / 2;
+  result += `@problems ${numProbs}\n`;
 
-	var teams = [];
-	var submissions = [];
+  var teams = [];
+  var submissions = [];
 
-	var tbody = boardTable.children[1];
-	for (var i = 0; i < tbody.children.length; i++) {
-		var teamRow = tbody.children[i];
+  var tbody = boardTable.children[1];
+  for (var i = 0; i < tbody.children.length; i++) {
+    var teamRow = tbody.children[i];
 
-		var teamName = teamRow.getElementsByClassName('team')[0].innerHTML;
-		var tdSchool = teamRow.getElementsByClassName('school');
-		if (tdSchool.length > 0) {
-			teamName += `（${tdSchool[0].innerHTML}）`;
-		}
-		teams.push(`@t ${i + 1},0,1,${teamName}`);
+    var teamName = teamRow.getElementsByClassName("team")[0].innerHTML;
+    var tdSchool = teamRow.getElementsByClassName("school");
+    if (tdSchool.length > 0) {
+      teamName += `（${tdSchool[0].innerHTML}）`;
+    }
+    teams.push(`@t ${i + 1},0,1,${teamName}`);
 
-		var probs = teamRow.getElementsByClassName('prob_d');
-		for (var j = 0; j < probs.length; j++) {
-			var prob = probs[j];
-			var probId = String.fromCharCode(65 + j);
-			if (prob.hasAttribute('rec')) {
-				var rec = prob.getAttribute('rec');
-				var time = 0, cnt = 0;
-				for (var k = 0; k < rec.length; k++) {
-					if (rec.charAt(k) == 'A' || rec.charAt(k) == 'R') {
-						cnt++;
-						submissions.push(`@s ${i + 1},${probId},${cnt},${time},${rec.charAt(k) == 'A' ? 'OK' : 'RJ'}`);
-						time = 0;
-					} else {
-						time = time * 10 + parseInt(rec.charAt(k));
-					}
-				}
-			}
-		}
-	}
+    var probs = teamRow.getElementsByClassName("prob_d");
+    for (var j = 0; j < probs.length; j++) {
+      var prob = probs[j];
+      var probId = String.fromCharCode(65 + j);
+      if (prob.hasAttribute("rec")) {
+        var rec = prob.getAttribute("rec");
+        var time = 0,
+          cnt = 0;
+        for (var k = 0; k < rec.length; k++) {
+          if (rec.charAt(k) == "A" || rec.charAt(k) == "R") {
+            cnt++;
+            submissions.push(
+              `@s ${i + 1},${probId},${cnt},${time},${
+                rec.charAt(k) == "A" ? "OK" : "RJ"
+              }`
+            );
+            time = 0;
+          } else {
+            time = time * 10 + parseInt(rec.charAt(k));
+          }
+        }
+      }
+    }
+  }
 
-	result += `@teams ${teams.length}\n`;
-	result += `@submissions ${submissions.length}\n`;
-	for (var i = 0; i < numProbs; i++) {
-		var probId = String.fromCharCode(65 + i);
-		result += `@p ${probId},${probId},20,0\n`;
-	}
-	teams.push(`@t ${teams.length + 1},0,1,Пополнить команду`);
+  result += `@teams ${teams.length}\n`;
+  result += `@submissions ${submissions.length}\n`;
+  for (var i = 0; i < numProbs; i++) {
+    var probId = String.fromCharCode(65 + i);
+    result += `@p ${probId},${probId},20,0\n`;
+  }
+  teams.push(`@t ${teams.length + 1},0,1,Пополнить команду`);
 
-	result += teams.join('\n') + '\n' + submissions.join('\n') + '\n';
-	return result;
+  result += teams.join("\n") + "\n" + submissions.join("\n") + "\n";
+  return result;
 }
--- static/js/index.js
+++ static/js/index.js
@@ -8,256 +8,276 @@ var teams = {};
 let timer = null;
 
 function getQueryString(name) {
-    let reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
-    let r = window.location.search.substr(1).match(reg);
-    if (r != null) {
-        return decodeURIComponent(r[2]);
-    };
-    return null;
- }
-
+  let reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+  let r = window.location.search.substr(1).match(reg);
+  if (r != null) {
+    return decodeURIComponent(r[2]);
+  }
+  return null;
+}
 
 function doTimeElapsed() {
-    var timeElapsed = $('#time_elapsed').attr('sec');
-    timeElapsed++;
-    $('#time_elapsed').attr('sec', timeElapsed);
-    $('#time_elapsed').text('Time Elapsed: ' + secondsToTime(timeElapsed));
+  var timeElapsed = $("#time_elapsed").attr("sec");
+  timeElapsed++;
+  $("#time_elapsed").attr("sec", timeElapsed);
+  $("#time_elapsed").text("Time Elapsed: " + secondsToTime(timeElapsed));
 
-    var timeLeft = $('#time_left').attr('sec');
-    timeLeft--;
-    $('#time_left').attr('sec', timeLeft);
-    $('#time_left').text('Time Left: ' + secondsToTime(timeLeft));
-    if (timeLeft <= 0) {
-        $('time_elapsed').text('');
-        $('#time_left').text('Contest Ended');
-        clearInterval(timer);
-    }
+  var timeLeft = $("#time_left").attr("sec");
+  timeLeft--;
+  $("#time_left").attr("sec", timeLeft);
+  $("#time_left").text("Time Left: " + secondsToTime(timeLeft));
+  if (timeLeft <= 0) {
+    $("time_elapsed").text("");
+    $("#time_left").text("Contest Ended");
+    clearInterval(timer);
+  }
 
-    var totalTime = timeElapsed + timeLeft;
+  var totalTime = timeElapsed + timeLeft;
 
-    $('#progress-bar').css('width', (timeElapsed / totalTime * 100) + '%');
-    if (timeLeft <= 0) {
-        $('#progress-bar').text('ENDED');
-    } else if (timeLeft <= 3600) {
-        $('#progress-bar').text('FROZEN');
-        $('#progress-bar').css('background-color', '#e0e0ff');
-    } else{
-        $('#progress-bar').text('RUNNING');
-        $('#progress-bar').css('background-color', '#4ad64a');
-
-    }
-
+  $("#progress-bar").css("width", (timeElapsed / totalTime) * 100 + "%");
+  if (timeLeft <= 0) {
+    $("#progress-bar").text("ENDED");
+  } else if (timeLeft <= 3600) {
+    $("#progress-bar").text("FROZEN");
+    $("#progress-bar").css("background-color", "#e0e0ff");
+  } else {
+    $("#progress-bar").text("RUNNING");
+    $("#progress-bar").css("background-color", "#4ad64a");
+  }
 }
 
 function fetchData() {
-    query = {};
-    var contestId = getQueryString('contestId');
-    var searchName = getQueryString('searchName');
-    if (searchName) {
-        query.searchName = searchName;
-    }
-    if (contestId == null) {
-        return;
-    }
-    $.ajax({
-        url: '/api/nowcoder/' + contestId,
-        type: 'GET',
-        data: query,
-        success: function(data) {
-            raw_data = data;
-            renderBoard();
-        }
-    });
+  query = {};
+  var contestId = getQueryString("contestId");
+  var searchName = getQueryString("searchName");
+  if (searchName) {
+    query.searchName = searchName;
+  }
+  if (contestId == null) {
+    return;
+  }
+  $.ajax({
+    url: "/api/nowcoder/" + contestId,
+    type: "GET",
+    data: query,
+    success: function (data) {
+      raw_data = data;
+      renderBoard();
+    },
+  });
 }
 
-function renderBoard(){
-    $('#time_elapsed').append(secondsToTime(timeElapsed));
-    $('#contest_title').text(raw_data.contest.contest_name);
-    $('#contest_time').text("Contest Time: " + secondsToDateTime(raw_data.contest.start_time) + " - " + secondsToDateTime(raw_data.contest.end_time));
-    $('#sponsors').text("Brought to you By " + raw_data.contest.organization + ".");
+function renderBoard() {
+  $("#time_elapsed").append(secondsToTime(timeElapsed));
+  $("#contest_title").text(raw_data.contest.contest_name);
+  $("#contest_time").text(
+    "Contest Time: " +
+      secondsToDateTime(raw_data.contest.start_time) +
+      " - " +
+      secondsToDateTime(raw_data.contest.end_time)
+  );
+  $("#sponsors").text(
+    "Brought to you By " + raw_data.contest.organization + "."
+  );
 
-    var curTime = parseInt(new Date().getTime() / 1000);
-    var timeLeft = raw_data.contest.end_time - curTime;
-    //console.log(timeLeft);
-    if (timeLeft <= 0) {
-        $('#time_left').text('Contest Ended');
-    } else {
-        $('#time_left').attr('sec', timeLeft);
-    }
+  var curTime = parseInt(new Date().getTime() / 1000);
+  var timeLeft = raw_data.contest.end_time - curTime;
+  //console.log(timeLeft);
+  if (timeLeft <= 0) {
+    $("#time_left").text("Contest Ended");
+  } else {
+    $("#time_left").attr("sec", timeLeft);
+  }
 
-    timeElapsed = parseInt(new Date().getTime() / 1000) - raw_data.contest.start_time;
-    $('#time_elapsed').attr('sec', timeElapsed);
-    if (curTime >= raw_data.contest.start_time && curTime <= raw_data.contest.end_time) {
-        timer = setInterval(doTimeElapsed, 1000);
-    } else if(curTime < raw_data.contest.start_time) {
-        $('#time_elapsed').text('Contest has not started yet');
-        $('#time_left').text('');
-        $('#progress-bar').css('width', '100%');
-        $('#progress-bar').text('PENDING');
-    } else {
-        $('#time_elapsed').text('');
-        $('#progress-bar').css('width', '100%');
-        $('#progress-bar').text('ENDED');
-    }
-    problemNum = raw_data.contest.problem_quantity;
-    for(var i = 0; i < problemNum; i++){
-        probs[raw_data.contest.problem[i].index] = {
-            ac: raw_data.contest.problem[i].accepted,
-            tries: raw_data.contest.problem[i].submission,
-            firstblood: 0,
-            problem_id: raw_data.contest.problem[i].problem_id,
-            title: raw_data.contest.problem[i].title
-        };
-    }
+  timeElapsed =
+    parseInt(new Date().getTime() / 1000) - raw_data.contest.start_time;
+  $("#time_elapsed").attr("sec", timeElapsed);
+  if (
+    curTime >= raw_data.contest.start_time &&
+    curTime <= raw_data.contest.end_time
+  ) {
+    timer = setInterval(doTimeElapsed, 1000);
+  } else if (curTime < raw_data.contest.start_time) {
+    $("#time_elapsed").text("Contest has not started yet");
+    $("#time_left").text("");
+    $("#progress-bar").css("width", "100%");
+    $("#progress-bar").text("PENDING");
+  } else {
+    $("#time_elapsed").text("");
+    $("#progress-bar").css("width", "100%");
+    $("#progress-bar").text("ENDED");
+  }
+  problemNum = raw_data.contest.problem_quantity;
+  for (var i = 0; i < problemNum; i++) {
+    probs[raw_data.contest.problem[i].index] = {
+      ac: raw_data.contest.problem[i].accepted,
+      tries: raw_data.contest.problem[i].submission,
+      firstblood: 0,
+      problem_id: raw_data.contest.problem[i].problem_id,
+      title: raw_data.contest.problem[i].title,
+    };
+  }
 
-    for(var i = 0; i < raw_data.runs.length; i++){
-        var team = raw_data.runs[i].team_id;
-        var prob = raw_data.runs[i].problem_id;
-        var time = raw_data.runs[i].timestamp;
-        var verd = raw_data.runs[i].status;
+  for (var i = 0; i < raw_data.runs.length; i++) {
+    var team = raw_data.runs[i].team_id;
+    var prob = raw_data.runs[i].problem_id;
+    var time = raw_data.runs[i].timestamp;
+    var verd = raw_data.runs[i].status;
 
-        if (typeof records[team] === 'undefined') {
-            records[team] = {
-                solved: 0,
-                penalty: 0
-            };
-        }
-        if (typeof records[team][prob] === 'undefined') {
-            records[team][prob] = {
-                status: '',
-                tries: 0,
-                ac: -1,
-                record: ''
-            };
-        }
-
-        if (verd == 'CORRECT') {
-            records[team][prob].record += parseInt(time / 1000) + 'A';
-            if (records[team][prob].ac < 0) {
-                records[team][prob].status = 'accepted';
-                records[team][prob].tries++;
-                records[team][prob].ac = parseInt(time / 1000 / 60);
-
-                records[team].solved++;
-                records[team].penalty += (
-                    parseInt(time / 1000 / 60) +
-                    (records[team][prob].tries - 1) * 20
-                );
-                //console.log(prob);
-                //probs[prob].tries++;
-                //probs[prob].ac++;
-                //console.log(probs[prob].firstblood);
-                if (probs[prob].firstblood == 0) {
-                    probs[prob].firstblood = team;
-                    records[team][prob].status = 'firstblood';
-                }
-            }
-        } else if (verd == 'PENDING') {
-            records[team][prob].record += parseInt(time / 1000) + 'P';
-            if (records[team][prob].ac < 0) {
-                records[team][prob].status = 'pending';
-                records[team][prob].tries++;
-
-                //probs[prob].tries++;
-            }
-        } else if (verd == 'INCORRECT') {
-            records[team][prob].record += parseInt(time / 1000) + 'R';
-            if (records[team][prob].ac < 0) {
-                records[team][prob].status = 'rejected';
-                records[team][prob].tries++;
-            }
-        }
-    }
-    
-
-    for (var key in records) {
-      sorted.push(key);
-    }
-    sorted.sort(function(x, y) {
-      if (records[x].solved > records[y].solved) {
-        return -1;
-      } else if (records[x].solved == records[y].solved) {
-        if (records[x].penalty < records[y].penalty) {
-          return -1;
-        } else if (records[x].penalty == records[y].penalty) {
-          return 0;
-        }
-      }
-      return 1;
-    });
-
-    for (var i = 0; i < problemNum; i++) {
-      $('#board thead tr:nth-child(1)').append(
-        '<th class="prob_h">' + String.fromCharCode(65 + i) + '</th>'
-      );
-    }
-    for (var i = 0; i < problemNum; i++) {
-      var prob = String.fromCharCode(65 + i);
-      $('#board thead tr:nth-child(2)').append(
-        '<th class="prob_h">' + probs[prob].ac + '/' + probs[prob].tries + '</th>'
-      );
-    }
-
-    teams = raw_data.teams;
-
-    for (var i = 0; i < sorted.length; i++) {
-      var team = sorted[i];
-      var tm = teams[team] || {
-        'type': 'unofficial',
-        'organization': '*',
-        'members': '*',
-        'team': '*'
+    if (typeof records[team] === "undefined") {
+      records[team] = {
+        solved: 0,
+        penalty: 0,
       };
-      var node = (
-        '<tr id="t' + team + '" class="type1">' +
-        '<td class="realrank">'+tm.extra["real_rank"]+'</td>' +
-        '<td class="rank"></td>' +
-        '<td class="schoolrank"></td>' +
-        '<td class="school">' + tm.organization + '</td>' +
-        '<td class="team" members="' + tm.team_id + '">' +
-        tm.name + '</td>' +
-        '<td class="solved">' + records[team].solved + '</td>' +
-        '<td class="penalty">' + records[team].penalty + '</td>'
-      );
+    }
+    if (typeof records[team][prob] === "undefined") {
+      records[team][prob] = {
+        status: "",
+        tries: 0,
+        ac: -1,
+        record: "",
+      };
+    }
 
-      for (var j = 0; j < problemNum; j++) {
-        var prob = String.fromCharCode(65 + j);
-        if (
-          typeof records[team][prob] === 'undefined' ||
-          records[team][prob].tries == 0
-        ) {
-          node += '<td class="prob_d untried">.</td>';
-        } else {
-          node += '<td class="prob_d ' + records[team][prob].status + '" rec="' +
-          records[team][prob].record + '">';
-          if (records[team][prob].ac < 0) {
-            node += records[team][prob].tries + '</td>';
-          } else {
-            node += records[team][prob].ac +
-            '(' + records[team][prob].tries + ')</td>';
-          }
+    if (verd == "CORRECT") {
+      records[team][prob].record += parseInt(time / 1000) + "A";
+      if (records[team][prob].ac < 0) {
+        records[team][prob].status = "accepted";
+        records[team][prob].tries++;
+        records[team][prob].ac = parseInt(time / 1000 / 60);
+
+        records[team].solved++;
+        records[team].penalty +=
+          parseInt(time / 1000 / 60) + (records[team][prob].tries - 1) * 20;
+        //console.log(prob);
+        //probs[prob].tries++;
+        //probs[prob].ac++;
+        //console.log(probs[prob].firstblood);
+        if (probs[prob].firstblood == 0) {
+          probs[prob].firstblood = team;
+          records[team][prob].status = "firstblood";
         }
       }
+    } else if (verd == "PENDING") {
+      records[team][prob].record += parseInt(time / 1000) + "P";
+      if (records[team][prob].ac < 0) {
+        records[team][prob].status = "pending";
+        records[team][prob].tries++;
 
-      node += '</tr>';
-      $('#board tbody').append(node);
+        //probs[prob].tries++;
+      }
+    } else if (verd == "INCORRECT") {
+      records[team][prob].record += parseInt(time / 1000) + "R";
+      if (records[team][prob].ac < 0) {
+        records[team][prob].status = "rejected";
+        records[team][prob].tries++;
+      }
     }
-    dorank();
-    if(scrollParam != null ){
-        scrollPage();
+  }
+
+  for (var key in records) {
+    sorted.push(key);
+  }
+  sorted.sort(function (x, y) {
+    if (records[x].solved > records[y].solved) {
+      return -1;
+    } else if (records[x].solved == records[y].solved) {
+      if (records[x].penalty < records[y].penalty) {
+        return -1;
+      } else if (records[x].penalty == records[y].penalty) {
+        return 0;
+      }
     }
-    
-    $("td.untried").each(function(i){
-    	$(this).attr("onclick","popdown()");
-    });
+    return 1;
+  });
 
-    $("td.prob_d").filter(":not(.untried)").each(function(i){
-    	$(this).attr("onclick","showsub(this)");
-    });
-    $("td.team").each(function(i){
-        $(this).attr("onclick","showteam(this)");
-    });
-    
-    
+  for (var i = 0; i < problemNum; i++) {
+    $("#board thead tr:nth-child(1)").append(
+      '<th class="prob_h">' + String.fromCharCode(65 + i) + "</th>"
+    );
+  }
+  for (var i = 0; i < problemNum; i++) {
+    var prob = String.fromCharCode(65 + i);
+    $("#board thead tr:nth-child(2)").append(
+      '<th class="prob_h">' + probs[prob].ac + "/" + probs[prob].tries + "</th>"
+    );
+  }
 
+  teams = raw_data.teams;
+
+  for (var i = 0; i < sorted.length; i++) {
+    var team = sorted[i];
+    var tm = teams[team] || {
+      type: "unofficial",
+      organization: "*",
+      members: "*",
+      team: "*",
+    };
+    var node =
+      '<tr id="t' +
+      team +
+      '" class="type1">' +
+      '<td class="realrank">' +
+      tm.extra["real_rank"] +
+      "</td>" +
+      '<td class="rank"></td>' +
+      '<td class="schoolrank"></td>' +
+      '<td class="school">' +
+      tm.organization +
+      "</td>" +
+      '<td class="team" members="' +
+      tm.team_id +
+      '">' +
+      tm.name +
+      "</td>" +
+      '<td class="solved">' +
+      records[team].solved +
+      "</td>" +
+      '<td class="penalty">' +
+      records[team].penalty +
+      "</td>";
+
+    for (var j = 0; j < problemNum; j++) {
+      var prob = String.fromCharCode(65 + j);
+      if (
+        typeof records[team][prob] === "undefined" ||
+        records[team][prob].tries == 0
+      ) {
+        node += '<td class="prob_d untried">.</td>';
+      } else {
+        node +=
+          '<td class="prob_d ' +
+          records[team][prob].status +
+          '" rec="' +
+          records[team][prob].record +
+          '">';
+        if (records[team][prob].ac < 0) {
+          node += records[team][prob].tries + "</td>";
+        } else {
+          node +=
+            records[team][prob].ac + "(" + records[team][prob].tries + ")</td>";
+        }
+      }
+    }
+
+    node += "</tr>";
+    $("#board tbody").append(node);
+  }
+  dorank();
+  if (scrollParam != null) {
+    scrollPage();
+  }
+
+  $("td.untried").each(function (i) {
+    $(this).attr("onclick", "popdown()");
+  });
+
+  $("td.prob_d")
+    .filter(":not(.untried)")
+    .each(function (i) {
+      $(this).attr("onclick", "showsub(this)");
+    });
+  $("td.team").each(function (i) {
+    $(this).attr("onclick", "showteam(this)");
+  });
 }
